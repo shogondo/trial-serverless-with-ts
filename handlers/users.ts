6a7c76535f2f82587dc0c7c82f1e03f30d042da2
Shou Takenaka <sasaminn@gmail.com>
2017-09-20 13:45:48 +0900

    Remove unused code

--- handlers/users.ts
+++ handlers/users.ts
@@ -6,20 +6,3 @@ export function users(event, context, callback) {
         model: new User()
     });
 }
-
-export function createUser(event, context, callback) {
-    Utils.processRequest(event, context, callback, (params) => {
-        let user = new User();
-        user.create(params);
-        return user.toJson();
-    });
-}
-
-export function updateUser(event, context, callback) {
-    Utils.processRequest(event, context, callback, (params) => {
-        let user = new User();
-        user.update(params);
-        return user.toJson();
-    });
-}
-
--- handlers/utils.ts
+++ handlers/utils.ts
@@ -18,22 +18,6 @@ export default class Utils {
         }
     }
 
-    public static processRequest(event, context, callback, action) {
-        console.log("Request", event);
-        try {
-            let params = this.convertEventDataToRequestParams(event);
-            let result = action(params);
-            let response = {
-                statusCode: 200,
-                body: JSON.stringify(result)
-            }
-            callback(null, response);
-        }
-        catch (error) {
-            callback(this.convertErrorToResponse(error));
-        }
-    }
-
     private static buildRequestParameters(event) {
         return {};
     }
@@ -49,15 +33,6 @@ export default class Utils {
                 return "update";
         }
     } 
-
-    private static convertEventDataToRequestParams(event) {
-        if (event && event.body) {
-            return JSON.parse(event.body);
-        }
-        else {
-            return {};
-        }
-    }
 
     private static convertErrorToResponse(error: Error) {
         return {
